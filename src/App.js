daf40c13be3282b1e2c6246f7152d3dd7cc600e4
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-09-23 18:19:18 +0500

    Routing

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
-import { Home } from './components'
-import Mypage from './components/Mypage'
-import Orders from './components/Orders'
-// import { Settings } from '@mui/icons-material';
-import Settings from "./components/Settings"
-import Products from './components/Products';
+import { Home,  Mypage, Orders, Settings, Products } from './components'
+
 
 const App = () => (
   <BrowserRouter>
     <Box>
-      
+     
       <Routes>
-         {/* <Route exact path='/' element={<Home />} /> */}
-         <Route exact path='/' element={<Mypage/>} />
-         {/* <Route exact path='/' element={<Orders/>} />  */}
-         {/* <Route exact path='/' element={<Products/>} /> */}
-         {/* <Route exact path='/' element={<Settings/>} />  */}
+          <Route exact path='/' element={<Home />} /> 
+         <Route  path='/mypage' element={<Mypage/>} />
+         <Route  path='/orders' element={<Orders/>} />  
+         <Route  path='/products' element={<Products/>} /> 
+         <Route  path='settings/' element={<Settings/>} />  
 
-
-
-         
       </Routes>
     </Box>
   </BrowserRouter>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
-export { default as Navbar  } from './Navbar'
-export { default as Home } from './Home'
+export { default as Navbar  } from './Navbar';
+export { default as Home } from './Home';
+export { default as Mypage } from './Mypage';
+export { default as Orders } from './Orders';
+export { default as Products } from './Products';
+export { default as Settings } from './Settings';
+
+
